122dfa4cee7b601535080cb5669ce0c635c7eb2a
monochromegane <dev.kuro.obi@gmail.com>
2014-02-02 18:38:37 +0900

    Added options

--- option/option.go
+++ option/option.go
@@ -3,4 +3,9 @@ package option
 type Option struct {
 	Image       string `short:"i" long:"image" required:"true" description:"docker image name"`
 	Concurrency int    `short:"n" default:"1" default-mask:"-" description:"Number of docker run concurrency"`
+	User        string `short:"u" default:"cargo"`
+	Group       string `short:"g"`
+	Dest        string `short:"d"`
+	Command     string `short:"c"`
+	WorkDir     string `short:"w" default:"~/cargo"`
 }
