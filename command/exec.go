9fb4af9cd3bf3640e662353b9c6e9ea8e4591572
monochromegane <dev.kuro.obi@gmail.com>
2014-02-02 13:14:00 +0900

    Added a command package.

---
+++ command/exec.go
@@ -0,0 +1,22 @@
+package command
+
+import (
+	"os/exec"
+)
+
+type Command struct {
+	Command string
+	Args    []string
+}
+
+type Result struct {
+	Output []byte
+	Err    error
+}
+
+func (self *Command) Exec(out chan *Result) {
+	cmd := exec.Command(self.Command, self.Args...)
+	result, err := cmd.Output()
+	out <- &Result{result, err}
+	close(out)
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	flags "github.com/jessevdk/go-flags"
+	"github.com/monochromegane/cargo/command"
 	"github.com/monochromegane/cargo/option"
 	"os"
 )
@@ -20,5 +21,14 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("It works!\n")
+
+	in := make(chan *command.Result)
+
+	command := command.Command{"docker", []string{"run", opts.Image, opts.Command}}
+	go command.Exec(in)
+
+	for result := range in {
+		fmt.Printf("%s, %s\n", result.Output, result.Err)
+	}
 
 }
