032db6c360406e31882e42d8d47319ff35a9c0b3
monochromegane <dev.kuro.obi@gmail.com>
2014-02-16 23:40:19 +0900

    Set default value to option.

--- cargo/option/option.go
+++ cargo/option/option.go
@@ -4,9 +4,9 @@ type Option struct {
 	Image       string `short:"i" long:"image" required:"true" description:"docker image name"`
 	Concurrency int    `short:"n" default:"1" default-mask:"-" description:"Number of docker run concurrency"`
 	User        string `short:"u" default:"cargo"`
-	Group       string `short:"g"`
+        Group       string `short:"g" default:"file-size"`
 	Dest        string `short:"d"`
 	Command     string `short:"c"`
-	WorkDir     string `short:"w" default:"~/cargo"`
+	WorkDir     string `short:"w" default:"/tmp/cargo"`
 	GoPackage   string `long:"go-package"`
 }
